d000d8ae99f6b3a7cd75ea7c70821c0c5a5f6aac
nguyenluongson06 <nguyenluongson06@gmail.com>
2024-06-17 07:40:40 +0700

    add option to card-detail page

--- card-detail.php
+++ card-detail.php
@@ -18,6 +18,22 @@ $related_cards = get_related_cards($card_id);
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $card["name"]; ?></title>
         <?php include_once ("components/style.php"); ?>
+        <script>
+        $(document).ready(function() {
+            $('#customMessageCheckbox').change(function() {
+                if ($(this).is(':checked')) {
+                    $('#customMessageText').show();
+                } else {
+                    $('#customMessageText').hide();
+                }
+            });
+        });
+        </script>
+        <style>
+        #customMessageText {
+            display: none;
+        }
+        </style>
     </head>
 
     <body>
@@ -48,18 +64,36 @@ $related_cards = get_related_cards($card_id);
                             <span style="font-size:1.25rem">$<?php echo $card["price"] ?></span>
                         </div>
                         <p class="lead"><?php echo $card["description"] ?></p>
-                        <form action="/add_to_cart.php" method="post" class="d-flex">
+                        <form action="/add_to_cart.php" method="post" class="d-flex flex-column">
                             <input type="hidden" name="id" value="<?php echo $card["id"] ?>" />
-                            <label for="inputQuantity" class="text-center"
-                                style="font-size:1.25rem; padding: 0.1rem; margin-right: 0.3rem">Quantity:
-                            </label>
-                            <input class="form-control text-center me-3" id="inputQuantity" name="inputQuantity"
-                                type="num" value="1" style="max-width: 3rem; margin-right: 1rem !important;">
-                            <button class="btn btn-success flex-shrink-0" type="submit"
-                                style="max-height:calc(1.5em + 0.75rem + 2px)">
-                                <i class="bi-cart-fill me-1"></i>
-                                Add to cart
-                            </button>
+                            <div class="d-flex flex-column">
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="giftWrapCheckbox"
+                                        name="giftWrap" value="1">
+                                    <label class="form-check-label" for="giftWrapCheckbox">Gift Wrap (extra $5)</label>
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="customMessageCheckbox"
+                                        name="customMessage" value="1">
+                                    <label class="form-check-label" for="customMessageCheckbox">Custom Message (extra
+                                        $3)</label>
+                                </div>
+                                <div class="form-group" id="customMessageText">
+                                    <label for="messageContent">Message Content:</label>
+                                    <textarea class="form-control" id="messageContent" name="messageContent"
+                                        rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-row align-items-center">
+                                <label for="inputQuantity" class="text-center m-1" style="font-size:1.25rem">Quantity:
+                                </label>
+                                <input class="form-control text-center m-3" id="inputQuantity" name="inputQuantity"
+                                    type="num" value="1" style="max-width: 3rem">
+                                <button class="btn btn-success flex-shrink-0 text-center p-3 fs-6" type="submit">
+                                    <i class="bi-cart-fill me-1"></i>
+                                    Add to cart
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -72,28 +106,28 @@ $related_cards = get_related_cards($card_id);
                 <h2 class="fw-bolder mb-4">Related products</h2>
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     <?php foreach ($related_cards as $item): ?>
-                        <div class="col-sm-6 col-md-4 col-lg-4" style="min-height: 98%;">
-                            <div class="box" style="min-height: 98%;">
-                                <div class="option_container">
-                                    <div class="options">
-                                        <a href="/card-detail.php?id=<?php echo $item["id"]; ?>" class="option1">
-                                            More info
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="img-box">
-                                    <img src="<?php echo $item["thumbnail_url"] ?>" alt="">
-                                </div>
-                                <div class="detail-box">
-                                    <h5>
-                                        <?php echo $item["name"] ?>
-                                    </h5>
-                                    <h6>
-                                        $<?php echo $item["price"] ?>
-                                    </h6>
+                    <div class="col-sm-6 col-md-4 col-lg-4" style="min-height: 98%;">
+                        <div class="box" style="min-height: 98%;">
+                            <div class="option_container">
+                                <div class="options">
+                                    <a href="/card-detail.php?id=<?php echo $item["id"]; ?>" class="option1">
+                                        More info
+                                    </a>
                                 </div>
                             </div>
+                            <div class="img-box">
+                                <img src="<?php echo $item["thumbnail_url"] ?>" alt="">
+                            </div>
+                            <div class="detail-box">
+                                <h5>
+                                    <?php echo $item["name"] ?>
+                                </h5>
+                                <h6>
+                                    $<?php echo $item["price"] ?>
+                                </h6>
+                            </div>
                         </div>
+                    </div>
                     <?php endforeach ?>
                 </div>
             </div>
