32e6ddf215a38aa384949006cdd5e9bb015fa186
nguyenluongson06 <nguyenluongson06@gmail.com>
2024-06-17 11:07:15 +0700

    add more detail in cart

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,14 +1,27 @@
 <?php
 require_once ("database/db.php");
+//3 arrays in session: cart(id, qty); gift_wrap_status(id, status); message(id, message)
+
 $cart = isset($_SESSION["cart"]) ? $_SESSION["cart"] : [];
+$gift_wrap_status = isset($_SESSION["gift_wrap"]) ? $_SESSION["gift_wrap"] : [];
+$message = isset($_SESSION["message"]) ? $_SESSION["message"] : [];
+
+
 $id = $_POST["id"];
 $qty = isset($_POST["inputQuantity"]) ? $_POST["inputQuantity"] : 1;
+$gift_wrap = isset($_POST["giftWrap"]) ? 1 : 0;
+$customMessage = isset($_POST['customMessage']) ? 1 : 0;
+$messageContent = isset($_POST['messageContent']) ? $_POST['messageContent'] : "";
 
-if (isset($cart[$id])) {
-    $cart[$id] = $cart[$id] + $qty;
+if (isset($cart[$id]) & $cart[$id]["qty"] > 0) {
+    $cart[$id]["qty"] = $cart[$id]["qty"] + $qty;
 } else {
     $cart[$id] = $qty;
+    $gift_wrap_status[$id] = $gift_wrap;
+    $message[$id] = $messageContent;
 }
 
 $_SESSION["cart"] = $cart;
+$_SESSION["gift_wrap"] = $gift_wrap_status;
+$_SESSION["message"] = $message;
 header("Location: /cart.php");
--- cart.php
+++ cart.php
@@ -1,6 +1,11 @@
 <?php
 require_once ("database/db.php");
+//get 3 arrays from session
 $cart = isset($_SESSION["cart"]) ? $_SESSION["cart"] : [];
+$gift_wrap_status = isset($_SESSION["gift_wrap"]) ? $_SESSION["gift_wrap"] : [];
+$message = isset($_SESSION["message"]) ? $_SESSION["message"] : [];
+
+//get cards' info
 $cards = get_cart();
 ?>
 <!DOCTYPE html>
@@ -43,79 +48,93 @@ $cards = get_cart();
                             </div>
                             <div class="card-body">
                                 <?php foreach ($cards as $item): ?>
-                                <!-- Single item -->
-                                <div class="row">
-                                    <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
-                                        <!-- Image -->
-                                        <div class="bg-image hover-overlay hover-zoom ripple rounded"
-                                            data-mdb-ripple-color="light">
-                                            <img src="<?php echo $item["thumbnail_url"] ?>" class="w-100"
-                                                alt="<?php echo $item["name"] ?>" />
-                                            <a href="#!">
-                                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)">
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <!-- Image -->
-                                    </div>
-
-                                    <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
-                                        <!-- Data -->
-                                        <p><strong><?php echo $item["name"] ?></strong></p>
-                                        <p>Manufacturer:
-                                            <?php echo get_manufacturer_detail($item["manufacturer_id"])["name"] ?>
-                                        </p>
-                                        <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                            class="btn btn-danger btn-sm me-1 mb-2" data-mdb-tooltip-init
-                                            title="Remove item">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                        <!-- Data -->
-                                    </div>
-
-                                    <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                                        <!-- Quantity -->
-                                        <div class="d-flex mb-4" style="max-width: 300px">
-                                            <button data-mdb-button-init="" data-mdb-ripple-init=""
-                                                class="btn btn-primary px-3 me-2 active"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                                                data-mdb-button-initialized="true" style="" aria-pressed="true">
-                                                <i class="fas fa-minus"></i>
-                                            </button>
-
-                                            <div data-mdb-input-init="" class="form-outline"
-                                                data-mdb-input-initialized="true">
-                                                <input id="form1" min="0" name="quantity"
-                                                    value="<?php echo $cart[$item["id"]] ?>" type="number"
-                                                    class="form-control active">
-                                                <label class="form-label" for="form1"
-                                                    style="margin-left: 0px;">Quantity</label>
-                                                <div class="form-notch">
-                                                    <div class="form-notch-leading" style="width: 9px;"></div>
-                                                    <div class="form-notch-middle" style="width: 52px;"></div>
-                                                    <div class="form-notch-trailing"></div>
-                                                </div>
+                                    <!-- Single item -->
+                                    <div class="row">
+                                        <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                                            <!-- Image -->
+                                            <div class="bg-image hover-overlay hover-zoom ripple rounded"
+                                                data-mdb-ripple-color="light">
+                                                <img src="<?php echo $item["thumbnail_url"] ?>" class="w-100"
+                                                    alt="<?php echo $item["name"] ?>" />
+                                                <a href="/card-detail.php?id=<?php echo $item["id"] ?>">
+                                                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)">
+                                                    </div>
+                                                </a>
                                             </div>
-
-                                            <button data-mdb-button-init="" data-mdb-ripple-init=""
-                                                class="btn btn-primary px-3 ms-2 active"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                                                data-mdb-button-initialized="true" style="" aria-pressed="true">
-                                                <i class="fas fa-plus"></i>
-                                            </button>
+                                            <!-- Image -->
                                         </div>
-                                        <!-- Quantity -->
 
-                                        <!-- Price -->
-                                        <p class="text-start text-md-center">
-                                            <strong>$<?php echo $item["price"] ?></strong>
-                                        </p>
-                                        <!-- Price -->
+                                        <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                                            <!-- Data -->
+                                            <p><strong><?php echo $item["name"] ?></strong></p>
+                                            <p>Manufacturer:
+                                                <?php echo get_manufacturer_detail($item["manufacturer_id"])["name"] ?>
+                                            </p>
+                                            <?php if (isset($gift_wrap_status[$item["id"]]) & $gift_wrap_status[$item["id"]] == 1): ?>
+                                                <div class="d-flex flex-row align-items-center">
+                                                    <i class="fa-solid fa-check p-1"></i>
+                                                    <p class="px-1 text-justify" style="margin: 0 !important">Gift Wrap</p>
+                                                </div>
+                                            <?php endif ?>
+                                            <?php if (isset($message[$item["id"]]) & $message[$item["id"]] != ""): ?>
+                                                <div class="d-flex flex-row align-items-center">
+                                                    <i class="fa-solid fa-check p-1"></i>
+                                                    <p class="px-1 text-justify" style="margin: 0 !important">Message:
+                                                        <?php echo $message[$item["id"]] ?>
+                                                    </p>
+                                                </div>
+                                            <?php endif ?>
+                                            <button type="button" data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-danger btn-sm me-1 mb-2" data-mdb-tooltip-init
+                                                title="Remove item">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </button>
+                                            <!-- Data -->
+                                        </div>
+
+                                        <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                                            <!-- Quantity -->
+                                            <div class="d-flex mb-4" style="max-width: 300px">
+                                                <button data-mdb-button-init="" data-mdb-ripple-init=""
+                                                    class="btn btn-primary px-3 me-2 active"
+                                                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                                                    data-mdb-button-initialized="true" style="" aria-pressed="true">
+                                                    <i class="fas fa-minus"></i>
+                                                </button>
+
+                                                <div data-mdb-input-init="" class="form-outline"
+                                                    data-mdb-input-initialized="true">
+                                                    <input id="form1" min="1" name="quantity"
+                                                        value="<?php echo $cart[$item["id"]] ?>" type="number"
+                                                        class="form-control active">
+                                                    <label class="form-label" for="form1"
+                                                        style="margin-left: 0px;">Quantity</label>
+                                                    <div class="form-notch">
+                                                        <div class="form-notch-leading" style="width: 9px;"></div>
+                                                        <div class="form-notch-middle" style="width: 52px;"></div>
+                                                        <div class="form-notch-trailing"></div>
+                                                    </div>
+                                                </div>
+
+                                                <button data-mdb-button-init="" data-mdb-ripple-init=""
+                                                    class="btn btn-primary px-3 ms-2 active"
+                                                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                                                    data-mdb-button-initialized="true" style="" aria-pressed="true">
+                                                    <i class="fas fa-plus"></i>
+                                                </button>
+                                            </div>
+                                            <!-- Quantity -->
+
+                                            <!-- Price -->
+                                            <p class="text-start text-md-center">
+                                                <strong>$<?php echo $item["price"] ?></strong>
+                                            </p>
+                                            <!-- Price -->
+                                        </div>
                                     </div>
-                                </div>
-                                <!-- Single item -->
+                                    <!-- Single item -->
 
-                                <hr class="my-4" />
+                                    <hr class="my-4" />
                                 <?php endforeach ?>
                             </div>
                         </div>
